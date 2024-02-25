384b91df51c97f1365a2ed48a390c9756238cf42
LarinNikita <larin_nikita_m@mail.ru>
2024-02-25 14:09:33 +0700

    empty search, boards and favorites

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,13 +3,25 @@
 import { useOrganization } from '@clerk/nextjs';
 
 import { EmptyOrg } from './_components/empty-org';
+import { BoardList } from './_components/board-list';
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+    searchParams: {
+        search?: string;
+        favorites?: string;
+    };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
     const { organization } = useOrganization();
 
     return (
         <div className="h-[calc(100%-80px)] flex-1 p-6">
-            {!organization ? <EmptyOrg /> : <p></p>}
+            {!organization ? (
+                <EmptyOrg />
+            ) : (
+                <BoardList orgId={organization.id} query={searchParams} />
+            )}
         </div>
     );
 };
