f46737588686b186d874b6de33f867994456990f
LarinNikita <larin_nikita_m@mail.ru>
2024-03-03 19:37:52 +0700

    fix sckeleton canvas

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,10 +1,25 @@
-export const Info = () => {
+'use client';
+
+import { useQuery } from 'convex/react';
+
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+
+interface InfoProps {
+    boardId: string;
+}
+
+export const Info = ({ boardId }: InfoProps) => {
+    const data = useQuery(api.board.get, {
+        id: boardId as Id<'boards'>,
+    });
+
     return (
         <div className="absolute left-2 top-2 flex h-12 items-center rounded-md bg-white px-1.5 shadow-md"></div>
     );
 };
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
     return (
         <div className="absolute left-2 top-2 flex h-12 w-[300px] items-center rounded-md bg-white px-1.5 shadow-md" />
     );
