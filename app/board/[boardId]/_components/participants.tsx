b854da2832bc6ec950aef25fd777cd6f0172e150
LarinNikita <larin_nikita_m@mail.ru>
2024-03-05 20:00:03 +0700

    room participants & user-avatar

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,6 +1,51 @@
+'use client';
+
+import { useOthers, useSelf } from '@/liveblocks.config';
+import { connectionIdToColor } from '@/lib/utils';
+
+import { UserAvatar } from './user-avatar';
+
+const MAX_SHOW_USERS = 2; //not including myself
+
 export const Participants = () => {
+    const users = useOthers();
+    const currentUser = useSelf();
+    const hasMoreUsers = users.length > MAX_SHOW_USERS;
+
     return (
-        <div className="absolute right-2 top-2 flex h-12 items-center rounded-md bg-white p-3 shadow-md"></div>
+        <div className="absolute right-2 top-2 flex h-12 items-center rounded-md bg-white p-3 shadow-md">
+            <div className="flex gap-x-2">
+                {users
+                    .slice(0, MAX_SHOW_USERS)
+                    .map(({ connectionId, info }) => {
+                        return (
+                            <UserAvatar
+                                borderColor={connectionIdToColor(connectionId)}
+                                key={connectionId}
+                                src={info?.picture}
+                                name={info?.name}
+                                fallback={info?.name?.[0] || 'T'}
+                            />
+                        );
+                    })}
+                {currentUser && (
+                    <UserAvatar
+                        borderColor={connectionIdToColor(
+                            currentUser.connectionId,
+                        )}
+                        src={currentUser.info?.picture}
+                        name={`${currentUser.info?.name} (You)`}
+                        fallback={currentUser.info?.name?.[0]}
+                    />
+                )}
+                {hasMoreUsers && (
+                    <UserAvatar
+                        name={`${users.length - MAX_SHOW_USERS} more`}
+                        fallback={`+`}
+                    />
+                )}
+            </div>
+        </div>
     );
 };
 
