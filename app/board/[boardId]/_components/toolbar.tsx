a3d4f6d71bfc64b3dca8a3df8f7f2344750fc482
LarinNikita <larin_nikita_m@mail.ru>
2024-03-08 11:31:09 +0700

    room toolbar

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,8 +1,78 @@
+import {
+    Circle,
+    MousePointer2,
+    Pencil,
+    Redo2,
+    Square,
+    StickyNote,
+    Type,
+    Undo2,
+} from 'lucide-react';
+import { ToolButton } from './tool-button';
+
 export const Toolbar = () => {
     return (
         <div className="absolute left-2 top-[50%] flex -translate-y-[50%] flex-col gap-y-4">
-            <div className="flex flex-col items-center gap-y-1 rounded-md bg-white p-1.5 shadow-md"></div>
-            <div className="flex flex-col items-center rounded-md bg-white p-1.5 shadow-md"></div>
+            <div className="flex flex-col items-center gap-y-1 rounded-md bg-white p-1.5 shadow-md">
+                <ToolButton
+                    label="Select"
+                    icon={MousePointer2}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+                <ToolButton
+                    label="Text"
+                    icon={Type}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+                <ToolButton
+                    label="Sticky note"
+                    icon={StickyNote}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+                <ToolButton
+                    label="Rectangle"
+                    icon={Square}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+                <ToolButton
+                    label="Ellipse"
+                    icon={Circle}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+                <ToolButton
+                    label="Pen"
+                    icon={Pencil}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+            </div>
+            <div className="flex flex-col items-center rounded-md bg-white p-1.5 shadow-md">
+                <ToolButton
+                    label="Undo"
+                    icon={Undo2}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+                <ToolButton
+                    label="Redo"
+                    icon={Redo2}
+                    onClick={() => {}}
+                    isActive={false}
+                    isDisabled={false}
+                />
+            </div>
         </div>
     );
 };
